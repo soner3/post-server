d8a0d3e4b63d078bc8dd0c435407e625a54ac331
soner3 <sonerastan@icloud.com>
2024-11-02 11:22:21 +0100

    Added AOP Logging

--- src/main/java/net/sonerapp/db_course_project/DbCourseProjectApplication.java
+++ src/main/java/net/sonerapp/db_course_project/DbCourseProjectApplication.java
@@ -2,8 +2,10 @@ package net.sonerapp.db_course_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class DbCourseProjectApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/net/sonerapp/db_course_project/core/aspects/LoggerAspect.java
@@ -0,0 +1,39 @@
+package net.sonerapp.db_course_project.core.aspects;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggerAspect {
+
+    private Logger log = LoggerFactory.getLogger(LoggerAspect.class.getName());
+
+    @Around("execution(* net.sonerapp.db_course_project.core.service..*.*(..)) || execution(* net.sonerapp.db_course_project.core.event..*.*(..))")
+    public Object logLogic(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info(joinPoint.getSignature().toString() + " method execution start");
+        Instant start = Instant.now();
+        Object result = joinPoint.proceed();
+        Instant end = Instant.now();
+        long executionDuration = Duration.between(start, end).toMillis();
+        log.info("Time took to execute the method: " + executionDuration + "ms");
+        log.info(joinPoint.getSignature().toString() + " method execution end");
+        return result;
+    }
+
+    @AfterThrowing(value = "execution(* net.sonerapp.db_course_project.core..*.*(..))", throwing = "ex")
+    public void logException(JoinPoint joinPoint, Exception ex) {
+        log.warn(joinPoint.getSignature() + " An exception is thrown due to: " + ex.getMessage());
+
+    }
+
+}
