cfea0ecb2cb8ab8293da4cc1e8d9d9686c86789b
soner3 <sonerastan@icloud.com>
2024-11-03 22:26:48 +0100

    Added Test Container

--- src/test/java/net/sonerapp/db_course_project/TestcontainersConfiguration.java
+++ src/test/java/net/sonerapp/db_course_project/TestcontainersConfiguration.java
@@ -1,8 +1,29 @@
 package net.sonerapp.db_course_project;
 
+import static org.mockito.Mockito.mock;
+
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
+
+    @Bean
+    @ServiceConnection
+    MySQLContainer<?> mysqlContainer() {
+        return new MySQLContainer<>(DockerImageName.parse("mysql:8.4"));
+
+    }
+
+    @Bean
+    @Primary
+    JavaMailSender mockJavaMailSender() {
+        return mock(JavaMailSender.class);
+    }
 
 }
