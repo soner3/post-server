ecf3f78eda908c7820ed5b6cccea5b1f300005e4
soner3 <sonerastan@icloud.com>
2024-11-05 16:25:10 +0100

    Changed title

--- src/main/java/net/sonerapp/db_course_project/infrastructure/doc/OpenApiConfig.java
+++ src/main/java/net/sonerapp/db_course_project/infrastructure/doc/OpenApiConfig.java
@@ -9,8 +9,8 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import net.sonerapp.db_course_project.infrastructure.security.jwt.JwtUtils;
 
-@OpenAPIDefinition(info = @Info(title = "DB Course Project API", license = @License(name = "MIT License", url = "https://opensource.org/license/mit"), description = "Documentation for all API endpoints of this project", version = "1.0"), servers = {
-        @Server(description = "LOCAL ENV", url = "http://localhost:8000") })
+@OpenAPIDefinition(info = @Info(title = "DB Course Project API from Soner, Luis and Jeanette", license = @License(name = "MIT License", url = "https://opensource.org/license/mit"), description = "Documentation for all API endpoints of this project", version = "1.0"), servers = {
+                @Server(description = "LOCAL ENV", url = "http://localhost:8000") })
 @SecurityScheme(name = "accessAuth", type = SecuritySchemeType.APIKEY, in = SecuritySchemeIn.COOKIE, paramName = JwtUtils.ACCESS_COOKIE_KEY)
 @SecurityScheme(name = "refreshAuth", type = SecuritySchemeType.APIKEY, in = SecuritySchemeIn.COOKIE, paramName = JwtUtils.REFRESH_COOKIE_KEY)
 public class OpenApiConfig {
